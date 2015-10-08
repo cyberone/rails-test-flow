62f8b163cc7ff1b1d98f3f11a183082953fb631e
Aleksey Popov <alopen@yandex.ru>
2015-10-08 11:43:22 +0600

    removed spec

--- spec/controllers/quiz_controller_spec.rb
+++ spec/controllers/quiz_controller_spec.rb
@@ -1,26 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe QuizController, type: :controller do
-
-  describe "GET #login" do
-    it "returns http success" do
-      get :login
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #answer" do
-    it "returns http success" do
-      get :answer
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #finish" do
-    it "returns http success" do
-      get :finish
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
